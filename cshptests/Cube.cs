7e1ce8489e495c973e0e489796550c3c21c11efd
bmircea <bmircea23@gmail.com>
2024-04-04 18:06:06 +0300

    changes

---
+++ cshptests/Cube.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cshptests
+{
+    class Cube
+    {
+        private List<Point> points;
+
+        Cube(List<Point> points)
+        {
+            this.points = points;
+        }
+
+
+    }
+}
---
+++ cshptests/GeoTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace cshptests
+{
+        [TestFixture]
+
+    public class GeoTests
+    {
+        [SetUp]
+        protected void SetUp()
+        {
+
+        }
+
+        
+    }
+}
