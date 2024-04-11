e333eb0b1ac03d8bbd9b272fb38c5b1d5de386a0
adnwhn <dianadoitrei@gmail.com>
2024-04-11 20:39:49 +0300

    restored Point class

---
+++ Compute/Point.cs
@@ -0,0 +1,18 @@
+﻿namespace Compute
+{
+    public class Point
+    {
+        private float x, y, z;
+        public Point(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public float GetX() { return x; }
+        public float GetY() { return y; }
+        public float GetZ() { return z; }
+    }
+
+}
